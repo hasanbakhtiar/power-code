ea4036bdd1d63d7ff09af287096065b7cb6238e9
HasanBakhtiar <hasan.doctype@gmail.com>
2024-02-01 17:54:22 +0400

    lastest

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,19 @@
+// debugger;    
+// console.log('Test js');
+// console.log('Test js');
+// console.log('Test js');
+// console.log('Test js');
+// console.log('Test js');
+// console.log('Test js');
+
+
+
+
+// console.log('Hello JavaScript!');
+
+// one line
+/**
+ * more 
+ * line 
+ * comment
+ */
---
+++ JavaScript/lesson2-dataTypes/js/script.js
@@ -0,0 +1,28 @@
+
+// Primitive DT
+// var a = 5; //number
+// var info = "Hello JavaScript"; //string
+// var data = true; //boolean
+
+
+// Composite DT
+// Array
+// const infoArr = ["Hello",true,5];
+// console.log(infoArr);
+
+// Object
+// const infoObj = {
+//     title:"Hello",
+//     price:2300
+// }
+// console.log(infoObj.title);
+
+
+// Special DT
+// var b = undefined;
+var c = null;
+
+console.log(c);
+
+
+
