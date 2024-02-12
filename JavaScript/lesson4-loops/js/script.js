08bee42a26d574f71776987067fcefff513d86be
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-02-12 12:51:21 +0400

    function

--- JavaScript/lesson4-loops/js/script.js
+++ JavaScript/lesson4-loops/js/script.js
@@ -20,3 +20,51 @@
 
 
 
+
+// const products = [
+//     {
+//         title:"Samsung s24",
+//         price:2300,
+//         stock:10,
+//         newDevice:true
+//     },
+//     {
+//         title:"Xiaomi 14",
+//         price:3300,
+//         stock:5,
+//         newDevice:true
+//     },
+//     {
+//         title:"Nokia 3310",
+//         price:100,
+//         stock:1,
+//         newDevice:false
+//     },
+//     {
+//         title:"ZenPhone",
+//         price:4300,
+//         stock:15,
+//         newDevice:false
+//     }
+// ];
+
+// for(let x of products){
+//     if (x.stock >= 5) {
+//        if (x.newDevice) {
+//         console.log(x.title);
+//        }
+//     }
+// }
+
+
+
+// for(let x in products){
+//     console.log(products[x].title);
+// }
+
+
+// for(let x = 0; x<products.length;x++){
+
+//     console.log(products[x].title);
+// }
+
--- JavaScript/lesson5-array-object/js/script.js
+++ JavaScript/lesson5-array-object/js/script.js
@@ -1,6 +1,6 @@
-const phoneBrands = ["Samsung","Xiaomi",[1,2,3,[true,'hello',555,['new data',['target']]],4,5],"ZenPhone","LG","Asus","Acer"];
+// const phoneBrands = ["Samsung","Xiaomi",[1,2,3,[true,'hello',555,['new data',['target']]],4,5],"ZenPhone","LG","Asus","Acer"];
 
-console.log(phoneBrands[2][3][3][1][0]);
+// console.log(phoneBrands[2][3][3][1][0]);
 
 // phoneBrands[6] = "new value"
 // phoneBrands['myindex'] = 'data';
@@ -11,4 +11,35 @@ console.log(phoneBrands[2][3][3][1][0]);
 // for(let count=0;count<phoneBrands.length;count++){
 // console.log(phoneBrands[count]);
 // }
+
+
+
+// const car = {
+//     img:"../img/logo.jpg",
+//     title:"BMW X5",
+//     price:30000,
+//     stock:10,
+//     color:['red','blue','green']
+// }
+// console.log(car.color[0]);
+
+
+
+// const info = ["Asus","Acer","Hp","Lenovo"];
+// info.push("Rog")
+// info.pop();
+// info.shift();
+// info.unshift("Rog");
+// console.log(info);
+
+
+// const info = "Hello";
+
+
+
+
+
+// console.log(info.length);
+
+
 
