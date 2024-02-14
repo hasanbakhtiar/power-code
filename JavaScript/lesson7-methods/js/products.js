c9ae77148604b70b9b8e4c21a03dbd422265c6b9
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-02-14 16:03:29 +0400

    methods

---
+++ JavaScript/lesson7-methods/js/products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        id:1,
+        title:"Samsung S23",
+        price:"2300",
+        stock:20
+    },
+    {
+        id:2,
+        title:"Xiaomi 14",
+        price:"1900",
+        stock:2
+    },
+    {
+        id:3,
+        title:"ZenPhone",
+        price:"1800",
+        stock:30
+    },
+    {
+        id:4,
+        title:"Oppo Find X",
+        price:"1300",
+        stock:5
+    }
+]
+
+
+export {products};
---
+++ JavaScript/lesson7-methods/js/script.js
@@ -0,0 +1,84 @@
+
+// "use strict";
+
+// let a = 5;
+// console.log(a);
+
+
+// const filteredProduct = products.find((item)=>{
+//   return item.stock > 10;
+// })
+
+
+// console.log(filteredProduct);
+
+// filteredProduct.map(item=>console.log(item.title))
+
+
+// products.forEach((item)=>{
+//     console.log(item.title +"-"+ i);
+// })
+
+
+
+
+
+
+// function infoOne(dataOne,prosess){
+//      prosess(dataOne);
+// }
+
+// infoOne(10,(comaingdata)=>{
+//   console.log(comaingdata);
+// })
+
+// import { products } from "./products.js";
+
+// for(let x of products){
+//   console.log(x);
+// }
+
+
+// const randomNumber = Math.random() * 100;
+// const yuvNumber = Math.round(randomNumber);
+// console.log(yuvNumber);
+
+
+// const children = ["Peri","Mehluqe","Ayse","Fidan","Irade","Gulnur"];
+
+
+// const randomValue = Math.random()*(children.length-1);
+// const rouNumber = Math.round(randomValue);
+// console.log(children[rouNumber]);
+
+
+
+// const info = Math.ceil(11.1);
+// console.log(info);
+
+
+// const fruits = ["Apple", "Banana", "Orange", "Kiwi", "Grape"];
+// const vegetable = ["Onion", "Potato", "Tomato", "Cucumber", "Balqabaq"];
+// // const info = vegetable.concat(fruits);
+//  fruits.splice(0,4);
+// console.log(fruits);
+
+
+
+
+// ternary
+
+// 5>10 ? console.log('he 5 dogurdanda 10dan kicikdir') :  console.log('yooo 5 dogurdanda 10dan boyukdur');
+
+
+
+
+console.log(prompt("deyer daxil edin",'test'));
+// window.prompt(); 
+
+// console.log(confirm("aboune olun"));
+// console.log(window.alert());
+
+
+
+
