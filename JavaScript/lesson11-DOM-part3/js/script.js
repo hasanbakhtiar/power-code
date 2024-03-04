48d56092b898fa0db3718b0dbdc4b5d1e5fe0de6
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-03-04 13:02:13 +0400

    local storgae

--- JavaScript/lesson11-DOM-part3/js/script.js
+++ JavaScript/lesson11-DOM-part3/js/script.js
@@ -1,30 +1,64 @@
 
-const text = document.querySelector('h1');
-const btn  = document.querySelector('button');
-
-btn.onclick=()=>{
-    if (btn.innerHTML ==='dark') {
-        btn.innerHTML = 'light';
-        text.className = 'dark';
-        localStorage.setItem('mode','dark');
-    }else{
-        text.className = 'light';
-        btn.innerHTML = 'dark';
-        localStorage.setItem('mode','light');
-
+const btn = document.querySelector('button');
+const li = document.querySelectorAll('li');
+btn.onclick = () => {
+    if (btn.innerHTML == "AZ") {
+        li[0].innerHTML = "Ana Sehife";
+        li[1].innerHTML = "Haqqimizda";
+        btn.innerHTML = 'EN';
+        localStorage.setItem('lang', 'az');
+    } else {
+        li[0].innerHTML = "Home";
+        li[1].innerHTML = "About";
+        btn.innerHTML = 'AZ';
+        localStorage.setItem('lang', 'en');
     }
 }
-if (localStorage.getItem('mode') === null) {
-    localStorage.setItem('mode','light')
-}else{
-    if (localStorage.getItem('mode')==='light') {
-        text.className = 'light';
-        btn.innerHTML = 'dark';
-    }else{
-        btn.innerHTML = 'light';
-        text.className = 'dark';
+
+if (localStorage.getItem('lang') === null) {
+    localStorage.setItem('lang', 'az');
+} else {
+    if (localStorage.getItem('lang') === 'az') {
+        li[0].innerHTML = "Ana Sehife";
+        li[1].innerHTML = "Haqqimizda";
+        btn.innerHTML = 'EN';
+    } else {
+        li[0].innerHTML = "Home";
+        li[1].innerHTML = "About";
+        btn.innerHTML = 'AZ';
     }
 }
+
+
+
+
+
+// const text = document.querySelector('h1');
+// const btn  = document.querySelector('button');
+
+// btn.onclick=()=>{
+//     if (btn.innerHTML ==='dark') {
+//         btn.innerHTML = 'light';
+//         text.className = 'dark';
+//         localStorage.setItem('mode','dark');
+//     }else{
+//         text.className = 'light';
+//         btn.innerHTML = 'dark';
+//         localStorage.setItem('mode','light');
+
+//     }
+// }
+// if (localStorage.getItem('mode') === null) {
+//     localStorage.setItem('mode','light')
+// }else{
+//     if (localStorage.getItem('mode')==='light') {
+//         text.className = 'light';
+//         btn.innerHTML = 'dark';
+//     }else{
+//         btn.innerHTML = 'light';
+//         text.className = 'dark';
+//     }
+// }
 
 
 
