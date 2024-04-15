7a3aa4720b15b0a4e645aea4dc96e6ff3259aca3
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-04-15 15:49:37 +0400

    github finder

--- ReactJS/lesson6-githubfinder/src/components/Search.jsx
+++ ReactJS/lesson6-githubfinder/src/components/Search.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 
-const Search = ({sendkeyword}) => {
-    const [keyword,setKeyword] = useState("");
-    const submitHandler =(e)=>{
-            e.preventDefault();
+const Search = ({ sendkeyword }) => {
+    const [keyword, setKeyword] = useState("");
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (!keyword) {
+            alert("please, fill input")
+        } else {
             sendkeyword(keyword);
+            setKeyword("");
+        }
     }
     return (
         <form onSubmit={submitHandler} className='d-flex align-items-center justify-content-center'>
             <div className="col-5">
-            <div className="input-group mt-3">
-                <input onChange={e=>setKeyword(e.target.value)} type="text" className="form-control" placeholder="Enter username"  />
-                <button className="btn btn-dark" type="submit" id="button-addon2">Search</button>
-            </div>
+                <div className="input-group mt-3">
+                    <input value={keyword} onChange={e => setKeyword(e.target.value)} type="text" className="form-control" placeholder="Enter username" />
+                    <button className="btn btn-dark" type="submit" id="button-addon2">Search</button>
+                </div>
             </div>
         </form>
     )
